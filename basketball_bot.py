467cba3c2c5dfee5b74c5602af3677eb8414486e
koshkakoshka1 <alexandr.kudlay@gmail.com>
2025-05-03 15:47:24 +0300

    Add files via upload

--- basketball_bot.py
+++ basketball_bot.py
@@ -3,13 +3,13 @@ from fastapi import FastAPI, Request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
-import uvicorn
 
 TOKEN = os.environ.get("TOKEN")
 BOT_URL = os.environ.get("BOT_URL")
 
 telegram_app = ApplicationBuilder().token(TOKEN).build()
 
+# Команды
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Напиши /top10 — и я пришлю тебе лучшие ставки дня 🏀")
 
@@ -19,6 +19,7 @@ async def top10(update: Update, context: ContextTypes.DEFAULT_TYPE):
 telegram_app.add_handler(CommandHandler("start", start))
 telegram_app.add_handler(CommandHandler("top10", top10))
 
+# FastAPI-приложение
 fastapi_app = FastAPI()
 
 @fastapi_app.on_event("startup")
@@ -43,6 +44,3 @@ async def handle_webhook(request: Request):
     except Exception as e:
         print("❌ Webhook error:", e)
         return {"error": str(e)}
-
-if __name__ == "__main__":
-    uvicorn.run("basketball_bot:fastapi_app", host="0.0.0.0", port=8080)
